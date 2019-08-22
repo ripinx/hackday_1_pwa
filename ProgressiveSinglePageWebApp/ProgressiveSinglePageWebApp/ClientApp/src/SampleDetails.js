76474a23d70eb4797a60dc9b747c5e21193402ba
harinik <harinik@inxsoftware.com>
2019-08-22 17:24:22 +0800

    Get user location

--- ProgressiveSinglePageWebApp/ProgressiveSinglePageWebApp/ClientApp/src/SampleDetails.js
+++ ProgressiveSinglePageWebApp/ProgressiveSinglePageWebApp/ClientApp/src/SampleDetails.js
@@ -2,8 +2,39 @@ import React from 'react';
 import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
 import SamplesHome from './SamplesHome';
 
-function SampleDetail() {
+class SampleDetail extends React.Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        latitude: null,
+        longitude: null
+    }
+  }
+
+  setLocation = (lat, long) => {
+    this.setState({ latitude: lat, longitude: long});
+  }
+
+  getLocation = () => {
+    if ("geolocation" in navigator) {
+
+      const setLocation = this.setLocation;
+
+      navigator.geolocation.getCurrentPosition(function(position) {
+        setLocation(position.coords.latitude, position.coords.longitude);
+      });
+    } else {
+      alert("Sorry, geolocation is not available in your browser.");
+    }
+  }
+
+  // getMap = () => {
+  //   this.props.history.push("map");
+  // }
+
+  render() {
     return (
         <div style={{textAlign: "left", padding: "10px"}}>
         <Form>
@@ -13,11 +44,17 @@ function SampleDetail() {
         </FormGroup>
         <FormGroup>
           <Label for="sampleLatitude">Latitude</Label>
-          <Input type="text" name="sampleLatitude" id="sampleLatitude"/>
+          <Input type="text" name="sampleLatitude" id="sampleLatitude" value={this.state.latitude}/>
         </FormGroup>
         <FormGroup>
           <Label for="sampleLongitude">Longitude</Label>
-          <Input type="text" name="sampleLongitude" id="sampleLongitude"/>
+          <Input type="text" name="sampleLongitude" id="sampleLongitude" value={this.state.longitude}/>
+          <Button style={{marginTop: "5px"}} color="primary"
+            onClick={this.getLocation}>
+            Use Current Location</Button>
+          {/* { navigator.onLine && <Button style={{marginTop: "5px", marginLeft: "5px"}} color="primary"
+            onClick={this.getMap}>
+            Get Location From Map</Button>} */}
         </FormGroup>
         <FormGroup>
           <Label for="sampleWeather">Weather</Label>
@@ -29,7 +66,7 @@ function SampleDetail() {
           </Input>
         </FormGroup>
         <FormGroup>
-          <Label for="sampleTemp">Temperature</Label>
+          <Label for="sampleTemp">Temperature (C)</Label>
           <Input type="number" name="sampleTemp" id="sampleTemp"/>
         </FormGroup>
         <FormGroup>
@@ -43,5 +80,6 @@ function SampleDetail() {
       </div>
     );
   }
+}
   
-  export default SampleDetail;
+export default SampleDetail;
